eabbca11b2e3e44a994e9d35a8acf8efd12f2c93
Ruan De Villiers <ruan.devilliers@nintex.com>
2023-08-15 00:00:06 +0200

    chore: removed data

--- src/basic_call.ts
+++ src/basic_call.ts
@@ -2,7 +2,11 @@ import { OpenAI } from "langchain/llms/openai";
 
 export const run = async () => {
   // temperature controls how random/creative the response is. It ranges from 0(deterministic) to 1(max creativity)
-  const model = new OpenAI({ temperature: 0.1 });
+  const model = new OpenAI({ temperature: 0.1,
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
+    azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION, });
 
   const res = await model.call("What is the capital city of France?");
   console.log({ res });
